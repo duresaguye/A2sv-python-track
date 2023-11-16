db4a1616021816e698883ab84c3f8a320413ff8a
Duresa Guye <dureguye2@gmail.com>
2023-11-17 02:36:38 +0300

    Time: 142 ms (85.44%) | Memory: 18.1 MB (73.30%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        a = len(g) - 1
+        b = len(s) - 1
+        maxs = 0
+
+        while a >= 0 and b >= 0:
+            if s[b] >= g[a]:
+                maxs += 1
+                a -= 1
+                b -= 1
+            else:
+                a -= 1
+        return maxs
+
+        
